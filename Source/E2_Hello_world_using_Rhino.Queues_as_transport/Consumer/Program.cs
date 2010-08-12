8c1c27fff1608f9a4b68c265e9a5187f5e341565
Bjoern Rochel <bjoern@bjro.de>
2010-08-12 20:50:08 +0200

    Hello world with Rhino.Queues is working

---
+++ Source/E2_Hello_world_using_Rhino.Queues_as_transport/Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Rhino.ServiceBus.Hosting;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Resetting queues");
+
+            if (Directory.Exists("Test.esent"))
+                Directory.Delete("Test.esent", true);
+
+            Console.WriteLine("Starting to listen for incoming messages ...");
+
+            var host = new DefaultHost();
+            host.Start<ConsumerBootStrapper>();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Source/E2_Hello_world_using_Rhino.Queues_as_transport/Sender/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Messages;
+using Rhino.ServiceBus;
+using Rhino.ServiceBus.Hosting;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new DefaultHost();
+            host.Start<SenderBootStrapper>();
+
+            var bus = host.Container.Resolve<IServiceBus>();
+
+            Console.WriteLine("Hit enter to send message");
+            Console.ReadLine();
+            Console.WriteLine("Sending message . . . ");
+
+            bus.Send(new HelloWorldMessage
+            {
+                Content = "Hello World!!!"
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
